9176a05ca39b8e6b4bc56b77bfe30c56cc6df197
Nicolas Posa <nicoposa57@gmail.com>
2024-01-14 23:43:28 -0300

    adding analitycs

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import About from "../components/About";
 import Skills from "../components/Skills";
 import Projects from "../components/Projects";
 import Contact from "../components/Contact";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function Home() {
   return (
@@ -21,6 +22,7 @@ export default function Home() {
       <Skills />
       <Projects/>
       <Contact />
+      <Analytics />
     </div>
   );
 }
